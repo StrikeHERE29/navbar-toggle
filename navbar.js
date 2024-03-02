627e8c36cd33c7136729fb5d039f1d4b2464bb03
StrikeHERE29 <vasilecristian2901@yahoo.com>
2024-03-02 11:28:50 +0200

    restructured the links in html, styled the show-class and added transition on toggle added functionality in js

---
+++ navbar.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector(".btn");
+let links = document.querySelector(".links");
+
+btn.addEventListener("click",function(){
+    
+    links.classList.toggle("show-links");
+    
+})
